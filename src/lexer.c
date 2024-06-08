19d9ed2adf0887999f2baa245a231b148c6dee26
Matshiro <mrmatshiro@gmail.com>
2024-06-08 23:06:45 +0200

    Added lexer skip whitespace and get next token

--- src/lexer.c
+++ src/lexer.c
@@ -1,5 +1,6 @@
 #include "include/lexer.h"
 #include <stdlib.h>
+#include <string.h>
 
 lexer_T* init_lexer(char* contents){
     lexer_T* lexer = calloc(1, sizeof(struct LEXER_STRUCT));
@@ -11,19 +12,51 @@ lexer_T* init_lexer(char* contents){
 }
 
 void lexer_advance(lexer_T* lexer){
-
+    if (lexer->c != '\0' && lexer->i < strlen(lexer->contents)){
+        lexer->i += 1;
+        lexer->c = lexer->contents[lexer->i];
+    }
 }
 
 void lexer_skip_whitespace(lexer_T* lexer){
-
+    while (lexer->c == ' ' || lexer->c == 10){
+        lexer_advance(lexer);
+    }
 }
 
 token_T* lexer_get_next_token(lexer_T* lexer){
-
+    while (lexer->c != '\0' && lexer->i < strlen(lexer->contents)){
+        while (lexer->c == ' ' || lexer->c == 10){
+            lexer_skip_whitespace(lexer);
+    }
+    switch (lexer->c){
+    case '=':
+        return lexer_advance_with_token(lexer, init_token(TOKEN_EQUALS, lexer_get_current_char_as_string(lexer)));
+        break;
+    case ';':
+        return lexer_advance_with_token(lexer, init_token(TOKEN_SEMI, lexer_get_current_char_as_string(lexer)));
+        break;
+    case '(':
+        return lexer_advance_with_token(lexer, init_token(TOKEN_LPAREN, lexer_get_current_char_as_string(lexer)));
+        break;
+    case ')':
+        return lexer_advance_with_token(lexer, init_token(TOKEN_RPAREN, lexer_get_current_char_as_string(lexer)));
+        break;
+    
+    default:
+        break;
+    }
+    }
 }
 
 token_T* lexer_collect_string(lexer_T* lexer){
 
+}
+
+token_T* lexer_advance_with_token(lexer_T* lexer, token_T* token){
+    lexer_advance(lexer);
+
+    return token;
 }
 
 char* lexer_get_current_char_as_string(lexer_T* lexer){
