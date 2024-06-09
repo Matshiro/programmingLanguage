68d8f9d6a010e06378f61c1590ea7fe86c801ecd
Daniel <matshiro@MatowyKomp.>
2024-06-09 21:12:54 +0200

    finished basic lexer, it's working while collecting tokens and printing them

--- src/lexer.c
+++ src/lexer.c
@@ -2,6 +2,8 @@
 #include "include/token.h"
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
+
 
 lexer_T* init_lexer(char* contents){
     lexer_T* lexer = calloc(1, sizeof(struct LEXER_STRUCT));
@@ -30,6 +32,9 @@ token_T* lexer_get_next_token(lexer_T* lexer){
         if (lexer->c == ' ' || lexer->c == 10)
             lexer_skip_whitespace(lexer);
 
+        if (isalnum(lexer->c))
+            return lexer_collect_id(lexer);
+
         if (lexer->c == '"')
             return lexer_collect_string(lexer);
 
@@ -48,9 +53,10 @@ token_T* lexer_get_next_token(lexer_T* lexer){
             break;
         
         default:
-            break;
+        break;
         }
     }
+    return (void*)0;
 }
 
 token_T* lexer_collect_string(lexer_T* lexer){
@@ -63,6 +69,8 @@ token_T* lexer_collect_string(lexer_T* lexer){
         char* s = lexer_get_current_char_as_string(lexer);
         value = realloc(value, (strlen(value) + strlen(s) + 1) *sizeof(char));
         strcat(value,s);
+
+        lexer_advance(lexer);
     }
 
     lexer_advance(lexer);
@@ -71,12 +79,26 @@ token_T* lexer_collect_string(lexer_T* lexer){
 }
 
 token_T* lexer_collect_id(lexer_T* lexer){
-    
+
+    char* value = calloc(1, sizeof(char));
+    value[0] = '\0';
+
+    while (isalnum(lexer->c)){
+        char* s = lexer_get_current_char_as_string(lexer);
+        value = realloc(value, (strlen(value) + strlen(s) + 1) *sizeof(char));
+        strcat(value,s);
+
+        lexer_advance(lexer);
+    }
+
+
+
+    return init_token(TOKEN_ID, value);
 }
 
 token_T* lexer_advance_with_token(lexer_T* lexer, token_T* token){
     lexer_advance(lexer);
-
+    
     return token;
 }
 
