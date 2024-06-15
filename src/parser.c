455325ceaea45a28c9ea6cfc5b2a40e04c781b80
Daniel <mrmatshiro@gmail.com>
2024-06-15 22:55:56 +0200

    lol

--- src/parser.c
+++ src/parser.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 
+// asdxd
 parser_T* init_parser(lexer_T* lexer){
     parser_T* parser = calloc(1, sizeof(struct PARSER_STRUCT));
     parser->lexer = lexer;
